640e484223309ff34ed68048d14d4919f049ddca
Yotamefr <yotamefr@gmail.com>
2024-01-02 14:08:31 +0200

    Updated fatcat-utils, worker now uses the new plugin system

--- fatcat_worker/__main__.py
+++ fatcat_worker/__main__.py
@@ -1,6 +1,15 @@
-from . import Worker
+import os
+import socket
+from fatcat_utils import FatCat
+from .__init__ import Worker
+
+
+def main():
+    fatcat = FatCat(f"{os.getenv('FATCAT_LOGGER_NAME', socket.gethostname())}")
+    worker = Worker(fatcat)
+    fatcat.add_plugin(worker)
+    fatcat.run()
 
 
 if __name__ == "__main__":
-    worker = Worker()
-    worker.run()
+    main()
--- fatcat_worker/worker.py
+++ fatcat_worker/worker.py
@@ -5,112 +5,91 @@ import shutil
 import os
 import pip
 from git.repo import Repo
-from fatcat_utils.rabbitmq import RabbitMQHandler, RABBIT_CONFIG
-from fatcat_utils.mongodb import DatabaseHandler
-from fatcat_utils.logger import generate_logger
+from fatcat_utils import FatCat
+from fatcat_utils.plugin import FatCatPlugin
+from fatcat_utils.rabbitmq import RABBIT_CONFIG
 from .config import load_config, IConfig
 
 
 RABBIT_CONFIG["failed_queue"] = os.getenv("FATCAT_RABBIT_WORKER_FAILED_QUEUE", "worker-failed")
 
-class Worker:
+class Worker(FatCatPlugin):
     """Worker class.
 
     Nothing much to document really. Just a class with all the worker functions
     """
-    logger: logging.Logger = generate_logger(f"FatCatWorker{os.getenv('FATCAT_WORKER_NAME')}Logger")
-    rabbit: RabbitMQHandler = RabbitMQHandler()
-    mongo: DatabaseHandler = DatabaseHandler()
     config: IConfig = load_config()
+    fatcat: FatCat
 
-    def run(self):
-        """The run function.
-
-        Call this function to start the worker.
-        """
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.start())
-        loop.call_later(5, self.check_listeners_status)
-        loop.run_forever()
-
-    async def start(self):
-        """THe start function.
-        
-        Sets up the mongo, clones the worker repo, 
-        initiates the listeners and connectes to the rabbit
-        """
-        self.logger.info("Setting up the Mongo environment")
-        await self.mongo.setup()
-        self.logger.info("Mongo has been set up")
-
-        self.logger.info("Inserting queues and schemas to the database")
+    def __init__(self, fatcat: FatCat):
+        self.fatcat = fatcat
+    
+    @FatCat.create_plugin_listener(on="ready")
+    async def on_ready(self):
+        self.fatcat.logger.info("Inserting queues and schemas to the database")
         await self.insert_queues_to_database()
-        self.logger.info("Finished inserting queues and schemas to the database")
+        self.fatcat.logger.info("Finished inserting queues and schemas to the database")
 
         if "url" in self.config["worker"]:
-            self.logger.info("Cloning the worker from Git")
+            self.fatcat.logger.info("Cloning the worker from Git")
             self.clone_worker()
-            self.logger.info("Done with the cloning")
+            self.fatcat.logger.info("Done with the cloning")
         elif "path" in self.config["worker"]:
-            self.logger.debug("Checking if worker exists")
+            self.fatcat.logger.debug("Checking if worker exists")
             if not (os.path.exists(self.config["worker"]["path"]) and os.path.isdir(self.config["worker"]["path"])):
-                self.logger.error(f"Worker path {self.config['worker']['path']} doesn't exist")
-            self.logger.debug("Worker path exists")
+                self.fatcat.logger.error(f"Worker path {self.config['worker']['path']} doesn't exist")
+            self.fatcat.logger.debug("Worker path exists")
         else:
-            self.logger.error("Key error in worker configuration")
+            self.fatcat.logger.error("Key error in worker configuration")
 
-        self.logger.info("Installing worker dependencies")
+        self.fatcat.logger.info("Installing worker dependencies")
         self.install_worker_deps()
-        self.logger.info("Done installing worker dependencies")
+        self.fatcat.logger.info("Done installing worker dependencies")
 
-        self.logger.info("Loading the worker")
+        self.fatcat.logger.info("Loading the worker")
         await self.load_worker()
-        self.logger.info("Done loading the worker")
-
-        self.logger.info("Starting up the Rabbit handler")
-        await self.rabbit.connect()
-        self.logger.info("Rabbit handler was been connected")
+        self.fatcat.logger.info("Done loading the worker")
 
     async def insert_queues_to_database(self):
         """Inserts the queues from the configuration to the database
         """
         for queue in self.config["queues"]:
-            self.logger.debug("Checking if queue document exists")
-            queue_doc = await self.mongo.get_queue_based_on_name(queue["queue_name"])
+            self.fatcat.logger.debug("Checking if queue document exists")
+            queue_doc = await self.fatcat.mongo.get_queue_based_on_name(queue["queue_name"])
 
             if queue_doc is None or queue_doc == {}:
-                self.logger.debug("Queue document doesn't exist. Creating new queue and schemas")
-                await self.mongo.create_queue({
+                self.fatcat.logger.debug("Queue document doesn't exist. Creating new queue and schemas")
+                await self.fatcat.mongo.create_queue({
                     "queue_name": queue["queue_name"],
                     "tags": queue["tags"]
                 }, queue["schemas"] if "schemas" in queue else None)
             else:
-                self.logger.debug("Queue document exists. Updating it")
-                await self.mongo.update_queue({
+                self.fatcat.logger.debug("Queue document exists. Updating it")
+                await self.fatcat.mongo.update_queue({
                     "queue_name": queue["queue_name"], "tags": queue["tags"]
                 })
-                self.logger.debug("Updating schemas in the database")
-                await self.mongo.update_schema(queue["schemas"] if "schemas" in queue else None, queue["queue_name"])
+                self.fatcat.logger.debug("Updating schemas in the database")
+                await self.fatcat.mongo.update_schema(queue["schemas"] if "schemas" in queue else None, queue["queue_name"])
 
     async def load_worker(self):
         """Loads the worker from the folder
         """
-        self.logger.debug("Loading the worker")
+        self.fatcat.logger.debug("Loading the worker")
         worker = importlib.import_module("worker", os.getcwd())
 
         load_worker = getattr(worker, "setup_worker")
         if asyncio.coroutines.iscoroutinefunction(worker):
-            self.logger.debug("Worker's setup function is a coroutine. Awaiting it")
+            self.fatcat.logger.debug("Worker's setup function is a coroutine. Awaiting it")
             await load_worker()
         else:
-            self.logger.debug("Worker's setup function is a normal function (probably). Calling it")
+            self.fatcat.logger.debug("Worker's setup function is a normal function (probably). Calling it")
             load_worker()
     
     def install_worker_deps(self):
         """Installing the worker dependencies (if exists)
         """
         if os.path.exists(os.path.join(os.getcwd(), "worker", "requirements.txt")) and not os.path.isdir(os.path.join(os.getcwd(), "worker", "requirements.txt")):
-            self.logger.debug("Installing worker dependencies")
+            self.fatcat.logger.debug("Installing worker dependencies")
             if hasattr(pip, "main"):
                 pip.main(["install", "-r", os.path.join(os.getcwd(), "worker", "requirements.txt")])
             else:
@@ -123,17 +102,17 @@ class Worker:
     def clone_worker(self):
         """Clones the worker from the git repo according to the configuration
         """
-        self.logger.debug("Checking if the worker path exists")
+        self.fatcat.logger.debug("Checking if the worker path exists")
         worker_path = os.path.join(os.getcwd(), "worker")
         if not os.path.exists(worker_path):
-            self.logger.debug("Worker path doesn't exist. Creating it")
+            self.fatcat.logger.debug("Worker path doesn't exist. Creating it")
             os.mkdir(worker_path)
         elif os.getenv("FATCAT_FORCE_GIT", "1") == "0": 
-            self.logger.debug("Worker path exists and I was asked not to clean. Exiting function")
+            self.fatcat.logger.debug("Worker path exists and I was asked not to clean. Exiting function")
             #  Continuing if it exists (this way you can just use it without git)
             return 
         else:
-            self.logger.debug("Worker path exists. Deleting it and recreating it")
+            self.fatcat.logger.debug("Worker path exists. Deleting it and recreating it")
             shutil.rmtree(worker_path)
             os.mkdir(worker_path)
 
@@ -150,15 +129,3 @@ class Worker:
             git_url = self.config["worker"]["url"]
         
         Repo.clone_from(git_url, worker_path)
-
-    def check_listeners_status(self):
-        """Checks if there are any active listeners. If not, kills the program.
-        """
-        loop = asyncio.get_event_loop()
-        self.logger.debug("Checking if there are no active listeners")
-        if len(self.rabbit._background_listeners) == 0:
-            self.logger.debug("No active listeners. Closing RabbitMQ and the async loop.")
-            loop.call_later(0.1, self.rabbit.close)  # Doesn't work with loop.run_until_complete cause of loop.run_forever
-            loop.stop()
-        else:
-            loop.call_later(5, self.check_listeners_status)
